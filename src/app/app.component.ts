caf4147412d3881ea5c653f096cb646140ed5092
Danilo <danilobojovicdb@gmail.com>
2023-04-28 13:17:43 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'cdk-playground';
+export class AppComponent implements OnInit {
+  title = 'ng-cdk-playground';
+  listItems: any[] | undefined;
+
+  constructor(private appService: AppService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    debugger;
+    this.appService.getProducts().subscribe({
+      next: (res) => {
+        this.listItems = res;
+      },
+      error: (e) => console.log('Error:', e.message),
+      complete: () => console.log('Success'),
+    });
+  }
 }
